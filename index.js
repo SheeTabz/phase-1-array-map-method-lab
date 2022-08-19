a14829d564be75bc33728500a771454d57c2cd30
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-19 10:03:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,46 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = tutorials.map(function(item){
+//   const words = item.split(" ");
+//     words.map(function(w) {
+//       w = w.charAt(0).toUpperCase() + w.substr(1);
+//       return w;
+//   })
+
+// })
+// console.log(titleCased)
+let newArr
+
+function titleCased(){
+  newArr=tutorials.map(item => {
+    let word=item.split(' ');
+    let word2=word.map(w=>`${w.charAt(0).toUpperCase()}${w.slice(1)}`).join(' ');
+    return word2;
+    
+    
+  });
+  return newArr;
 }
+console.log(titleCased())
+
+
+
+
+
+
+
+// const robots = [
+//   { name: "Johnny 5", modes: 5, isActivated: false },
+//   { name: "C3PO", modes: 3, isActivated: false },
+//   { name: "Sonny", modes: 2.5, isActivated: false },
+//   { name: "Baymax", modes: 1.5, isActivated: false },
+// ];
+
+
+// const updatedRObots = robots.map(function(robot){
+//   return Object.assign({}, robot, {modes: robot.modes * 2, isActivated: true})
+// })
+// console.log(updatedRObots)
+// console.log(robots)
+
